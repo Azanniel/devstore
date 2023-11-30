8ac7407530f7217ef69bef05bbda326a9613d90d
azanniel <leo.azannielttt@gmail.com>
2023-11-29 22:14:28 -0400

    🚸 add favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-gray-800 font-sans text-zinc-200">
+    <div className="flex h-screen w-screen items-center justify-center bg-gray-800 text-zinc-200">
       <h1>Hello world</h1>
     </div>
   )
