548f0861215ffa2499fb81335a5cc21a0be11094
azanniel <leo.azannielttt@gmail.com>
2023-11-30 23:40:13 -0400

    📸 add layout for app

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex h-screen w-screen items-center justify-center bg-gray-800 text-zinc-200">
-      <h1>Hello world</h1>
-    </div>
-  )
-}
