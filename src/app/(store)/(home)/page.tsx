e524958244a447ce78735d8044760c9e29f5d40a
azanniel <leo.azannielttt@gmail.com>
2023-12-19 21:54:41 -0400

    ✨ add search page layout

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -49,33 +49,35 @@ export default async function Home() {
         </div>
       </Link>
 
-      {otherProducts.map((product) => (
-        <Link
-          key={product.id}
-          href={`/product/${product.slug}`}
-          className="group relative col-span-3 row-span-3 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
-        >
-          <Image
-            className="transition-transform duration-500 group-hover:scale-105"
-            src={product.image}
-            width={920}
-            height={920}
-            quality={100}
-            alt=""
-          />
+      {otherProducts.map((product) => {
+        return (
+          <Link
+            key={product.id}
+            href={`/product/${product.slug}`}
+            className="group relative col-span-3 row-span-3 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
+          >
+            <Image
+              className="transition-transform duration-500 group-hover:scale-105"
+              src={product.image}
+              width={920}
+              height={920}
+              quality={100}
+              alt=""
+            />
 
-          <div className="absolute bottom-10 right-10 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-            <span className="truncate text-sm">{product.title}</span>
-            <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-              {product.price.toLocaleString('pt-BR', {
-                style: 'currency',
-                currency: 'BRL',
-                maximumFractionDigits: 0,
-              })}
-            </span>
-          </div>
-        </Link>
-      ))}
+            <div className="absolute bottom-10 right-10 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+              <span className="truncate text-sm">{product.title}</span>
+              <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+                {product.price.toLocaleString('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                  maximumFractionDigits: 0,
+                })}
+              </span>
+            </div>
+          </Link>
+        )
+      })}
     </div>
   )
 }
